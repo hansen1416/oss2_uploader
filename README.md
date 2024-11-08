994ca583ab5403e4cc506af356d2bf6b64959f17
Hanson Han <105476@grundfos.com>
2024-11-08 13:23:04 +0800

    1

--- README.md
+++ README.md
@@ -13,14 +13,11 @@ only executable under `if __name__ == "__main__":`
 ```
 if __name__ == "__main__":
 
-    from oss2_uploader import folder_uploader
+    from oss2_uploader import folder_uploader_sync
 
-    folder_uploader(, , 
-        folder_path="./test-folder,
-        bucket_name="test-bucket",
-        oss_endpoint="oss-ap-southeast-1.aliyuncs.com",
-        oss_path="pathname",
+    folder_uploader_sync(
+        os.path.join(os.path.expanduser("~"), "Documents"),
+        "bucket_name",
+        "oss-ap-southeast-1.aliyuncs.com",
     )
 ```
-
-if `oss_path` is omitted, will use the folder name as prefix in oss
--- src/oss2_uploader/main.py
+++ src/oss2_uploader/main.py
@@ -1,6 +1,8 @@
 from multiprocessing import Process
 import os
 import sys
+import pathlib
+from typing import List
 
 import tqdm
 import oss2
@@ -202,6 +204,53 @@ def folder_downloader(bucket_name, oss_endpoint, oss_prefix, target_dir):
         process.join()
 
 
+def folder_uploader_sync(folder_path, bucket_name, oss_endpoint):
+
+    folder_path = os.path.abspath(folder_path)
+
+    if not os.path.exists(folder_path):
+        print(f"{folder_path} does not exist")
+        sys.exit(1)
+
+    path_delimiter = os.path.sep
+    all_oos_path = []
+
+    # get all files in the folder recursively
+    all_files: List[str] = [
+        str(f) for f in pathlib.Path(folder_path).rglob("*") if f.is_file()
+    ]
+
+    for filepath in all_files:
+
+        # remove the prefix `folder_path` from the file path
+        sub_path = filepath[len(folder_path) + 1 :]
+
+        all_oos_path.append("/".join(sub_path.split(path_delimiter)))
+
+    # create a bucket
+    auth = oss2.ProviderAuth(EnvironmentVariableCredentialsProvider())
+    bucket = oss2.Bucket(auth, oss_endpoint, bucket_name, connect_timeout=30)
+
+    print(f"Total files to upload: {len(all_files)}")
+
+    # upload all files
+    for i, filepath in enumerate(tqdm.tqdm(all_files, desc="Uploading files")):
+        if not os.path.isfile(filepath):
+            continue
+
+        target_path = all_oos_path[i]
+
+        # check if the file already exists in oss
+        if bucket.object_exists(target_path):
+            # print(f"{self.process_idx}: {target_path} already exists in oss")
+            continue
+
+        bucket.put_object_from_file(
+            target_path,
+            filepath,
+        )
+
+
 if __name__ == "__main__":
 
     def load_env_from_file(file_path: str = None):
@@ -238,15 +287,8 @@ if __name__ == "__main__":
         file_path=os.path.join(os.path.dirname(__file__), "..", "..", ".env")
     )
 
-    # folder_uploader(
-    #     os.path.join(os.path.expanduser("~"), "Documents", "test111"),
-    #     "pose-daten",
-    #     "oss-ap-southeast-1.aliyuncs.com",
-    # )
-
-    folder_downloader(
-        "pose-daten",
-        "oss-ap-southeast-1.aliyuncs.com",
-        "test111",
+    folder_uploader_sync(
         os.path.join(os.path.expanduser("~"), "Documents", "test111"),
+        "video2motion-pretrain",
+        "oss-ap-southeast-1.aliyuncs.com",
     )
